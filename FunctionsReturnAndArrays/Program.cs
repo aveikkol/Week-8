573ae40f40038967917198e856b05c67e29bdf07
angelina-alice veikkolainen <aveikkol@tktk.ee>
2021-12-12 14:34:50 +0200

    week-8

---
+++ FunctionsReturnAndArrays/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FunctionsReturnAndArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayRandomFood();
+            DisplayRandomDrink();
+            DisplayRandomMovie();
+        }
+        private static void DisplayRandomFood()
+        {
+            string[] foods = { "chicken wings", "pizza", "sushi", "popcorn", "salad" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, foods.Length);
+
+            string randomFood = foods[randomIndex];
+            Console.WriteLine($"Computer picked: {randomFood}");
+        }
+
+        private static void DisplayRandomDrink()
+        {
+            string[] drinks = { "apple juice", "coke", "fanta", "sprite", "water" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, drinks.Length);
+
+            string randomDrink = drinks[randomIndex];
+            Console.WriteLine($"Computer picked: {randomDrink}");
+
+        }
+
+        private static void DisplayRandomMovie()
+        {
+            string[] movies = { "do not breathe", "scream", "emma", "you", "christmas tree" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, movies.Length);
+
+            string randomMovie = movies[randomIndex];
+            Console.WriteLine($"Computer picked: {randomMovie}");
+        }
+    }
+}
